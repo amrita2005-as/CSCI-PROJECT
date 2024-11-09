2d7ec77de0ddf4d241d6bb5a7fb4630d77e48302
Amritaa Shankar <amriiii365@gmail.com>
2024-11-09 20:13:34 +0400

    Week 9 (Dead-end with maximum light value).c

--- e-puck_avoid_obstacles.c
+++ e-puck_avoid_obstacles.c
@@ -4,10 +4,12 @@
 #include <webots/motor.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>  
 #include <webots/gps.h>
 
 #define TIME_STEP 64  // Time step for simulation in ms
 #define MAX_SPEED 6.28 // Maximum velocity of motor
+#define GPS_THRESHOLD 0.07 // Tolerance for GPS position comparison
 
 int main(int argc, char **argv) {
   wb_robot_init();
@@ -21,7 +23,7 @@ int main(int argc, char **argv) {
 
   WbDeviceTag ds_left = wb_robot_get_device("ps0");
   WbDeviceTag ds_right = wb_robot_get_device("ps1");
-  WbDeviceTag light_sensor = wb_robot_get_device("ls0"); 
+  WbDeviceTag light_sensor = wb_robot_get_device("ls0");
 
   WbDeviceTag prox_sensors[8];
   char prox_sensor_name[50];
@@ -35,13 +37,14 @@ int main(int argc, char **argv) {
   wb_distance_sensor_enable(ds_left, TIME_STEP);
   wb_distance_sensor_enable(ds_right, TIME_STEP);
 
-  double left_speed = MAX_SPEED;
-  double right_speed = MAX_SPEED;
-
   // Arrays for storing dead-end data (coordinates and light intensity)
   double dead_end_coordinates[10][3];  // Store coordinates for up to 10 dead-ends
   double dead_end_light[10];            // Store light intensity for up to 10 dead-ends
   int dead_end_count = 0;
+
+  // Variables for navigating to the brightest dead-end
+  double max_light_intensity = 0.0;
+  int brightest_dead_end = -1;
 
   // Main loop
   while (wb_robot_step(TIME_STEP) != -1) {
@@ -57,7 +60,7 @@ int main(int argc, char **argv) {
     double light_value = wb_light_sensor_get_value(light_sensor);
     printf("Light Sensor Value: %f\n", light_value);
 
-    // Check if the robot is in a dead-end (similar to previous logic)
+    // Check if the robot is in a dead-end (both walls detected)
     if (front_wall && left_wall) {
       const double *gps_values = wb_gps_get_values(gps);
 
@@ -66,13 +69,53 @@ int main(int argc, char **argv) {
         dead_end_coordinates[dead_end_count][0] = gps_values[0];
         dead_end_coordinates[dead_end_count][1] = gps_values[1];
         dead_end_coordinates[dead_end_count][2] = gps_values[2];
-        dead_end_light[dead_end_count] = light_value;  // Store the light intensity
+        dead_end_light[dead_end_count] = light_value;
 
         printf("Dead-end %d reached at coordinates: (%f, %f, %f)\n", dead_end_count + 1, 
                gps_values[0], gps_values[1], gps_values[2]);
         printf("Light intensity at dead-end %d: %f\n", dead_end_count + 1, light_value);
 
         dead_end_count++;
+
+        // Update the brightest dead-end if current one has higher light intensity
+        if (light_value > max_light_intensity) {
+          max_light_intensity = light_value;
+          brightest_dead_end = dead_end_count - 1; // store the index of the brightest dead-end
+        }
+      }
+    }
+
+    // Check if we need to navigate to the brightest dead-end
+    if (brightest_dead_end >= 0) {
+      const double *gps_values = wb_gps_get_values(gps);
+
+      // Compare the current GPS position with the brightest dead-end coordinates
+      double distance_to_brightest = sqrt(pow(gps_values[0] - dead_end_coordinates[brightest_dead_end][0], 2) +
+                                          pow(gps_values[1] - dead_end_coordinates[brightest_dead_end][1], 2) +
+                                          pow(gps_values[2] - dead_end_coordinates[brightest_dead_end][2], 2));
+
+      // If close enough to the brightest dead-end, stop the robot
+      if (distance_to_brightest < GPS_THRESHOLD) {
+        printf("\n\nReached the brightest dead-end with light intensity: %f\n\n", max_light_intensity);
+        wb_motor_set_velocity(left_motor, 0);
+        wb_motor_set_velocity(right_motor, 0);
+        break; // Exit the loop after reaching the target
+      } else {
+        // Move towards the brightest dead-end (basic navigation)
+        double left_speed = MAX_SPEED;
+        double right_speed = MAX_SPEED;
+
+        // Navigate towards the brightest dead-end
+        if (gps_values[0] < dead_end_coordinates[brightest_dead_end][0]) {
+          left_speed = MAX_SPEED / 2;
+          right_speed = MAX_SPEED;
+        } else if (gps_values[0] > dead_end_coordinates[brightest_dead_end][0]) {
+          left_speed = MAX_SPEED;
+          right_speed = MAX_SPEED / 2;
+        }
+
+        wb_motor_set_velocity(left_motor, left_speed);
+        wb_motor_set_velocity(right_motor, right_speed);
       }
     }
 
